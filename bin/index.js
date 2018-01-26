973ff48da76866ec118706dacc2a2847c090dbaf
Ben Sharafian <benms64@gmail.com>
2018-01-26 12:03:54 -0800

    feat: load details from file

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,19 @@
 #!/usr/bin/env node
+const fs = require('fs')
 
 require('yargs')
   .option('parent', {
-    required: true,
     description: 'BTP host of your parent connector, e.g. "client.scyl.la"'
   })
   .option('id', {
     description: 'ID for this connection. allows you to open several channels to same host.'
   })
   .option('secret', {
-    required: true,
     description: 'XRP secret, "s..."'
+  })
+  .option('file', {
+    alias: 'f',
+    description: 'JSON file containing options'
   })
   .option('address', {
     description: 'XRP address. Can be derived from secret.',
@@ -21,12 +24,21 @@ require('yargs')
     description: 'Rippled server. Uses S1 server provided by Ripple by default.'
   })
   .command('start', 'launch moneyd', {}, argv => {
-    process.env.PARENT_BTP_HOST = argv.parent
-    process.env.NONCE = argv.id
-    process.env.XRP_SECRET = argv.secret
-    process.env.XRP_ADDRESS = argv.address
-    process.env.XRP_SERVER = argv.rippled
-    process.env.DEBUG = 'ilp*,connector*'
+    if (argv.file) {
+      const file = JSON.parse(fs.readFileSync(argv.file).toString())
+      process.env.PARENT_BTP_HOST = file.parent || ''
+      process.env.NONCE = file.id || ''
+      process.env.XRP_SECRET = file.secret || ''
+      process.env.XRP_ADDRESS = file.address || ''
+      process.env.XRP_SERVER = file.rippled || ''
+    } else {
+      process.env.PARENT_BTP_HOST = argv.parent
+      process.env.NONCE = argv.id
+      process.env.XRP_SECRET = argv.secret
+      process.env.XRP_ADDRESS = argv.address
+      process.env.XRP_SERVER = argv.rippled
+    }
+
     console.log('set environment; starting moneyd')
     require('..')
   })
@@ -36,12 +48,21 @@ require('yargs')
       default: 1000
     }
   }, argv => {
-    process.env.PARENT_BTP_HOST = argv.parent
-    process.env.NONCE = argv.id
-    process.env.XRP_SECRET = argv.secret
-    process.env.XRP_ADDRESS = argv.address
-    process.env.XRP_SERVER = argv.rippled
-    process.env.AMOUNT = argv.amount
+    if (argv.file) {
+      const file = JSON.parse(fs.readFileSync(argv.file).toString())
+      process.env.PARENT_BTP_HOST = file.parent || ''
+      process.env.NONCE = file.id || ''
+      process.env.XRP_SECRET = file.secret || ''
+      process.env.XRP_ADDRESS = file.address || ''
+      process.env.XRP_SERVER = file.rippled || ''
+    } else {
+      process.env.PARENT_BTP_HOST = argv.parent
+      process.env.NONCE = argv.id
+      process.env.XRP_SECRET = argv.secret
+      process.env.XRP_ADDRESS = argv.address
+      process.env.XRP_SERVER = argv.rippled
+    }
+
     console.log('set environment; starting moneyd')
     require('./settle.js')
   })
