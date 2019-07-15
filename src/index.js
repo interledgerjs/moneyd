64ec23cf2f48b0eda2a454dcf1348c48bfee6f75
DonChangfoot <donovanchangfoot@gmail.com>
2019-07-15 11:30:12 +0200

    add moneyd-uplink-btp and extra env vari * env vari to configure mini account port * env vari to configure asset code and scale when using local mode

--- src/index.js
+++ src/index.js
@@ -11,16 +11,16 @@ const uplinkModuleNames = {
   xrp: 'moneyd-uplink-xrp',
   eth: 'moneyd-uplink-eth',
   coil: 'moneyd-uplink-coil',
-  http: 'moneyd-uplink-http'
-  // btp: 'moneyd-uplink-btp'
+  http: 'moneyd-uplink-http',
+  btp: 'moneyd-uplink-btp'
 }
 
 const uplinks = {
   xrp: maybeRequire('moneyd-uplink-xrp'),
   eth: maybeRequire('moneyd-uplink-eth'),
   coil: maybeRequire('moneyd-uplink-coil'),
-  http: maybeRequire('moneyd-uplink-http')
-  // btp: maybeRequire('moneyd-uplink-btp')
+  http: maybeRequire('moneyd-uplink-http'),
+  btp: maybeRequire('moneyd-uplink-btp')
 }
 
 function maybeRequire (pkg) {
@@ -76,7 +76,7 @@ class Moneyd {
           options: {
             wsOpts: {
               host: process.env.MONEYD_BIND_IP || 'localhost',
-              port: 7768
+              port: process.env.MONEYD_BIND_PORT || 7768
             },
             allowedOrigins: this.allowedOrigins
           }
@@ -99,8 +99,8 @@ class Moneyd {
         local: {
           relation: 'child',
           plugin: 'ilp-plugin-mini-accounts',
-          assetCode: 'XRP',
-          assetScale: 9,
+          assetCode: process.env.MONEYD_BIND_ASSET_CODE || 'XRP',
+          assetScale: process.env.MONEYD_BIND_ASSET_SCALE || 9,
           balance: {
             minimum: '-Infinity',
             maximum: 'Infinity',
@@ -109,7 +109,7 @@ class Moneyd {
           options: {
             wsOpts: {
               host: process.env.MONEYD_BIND_IP || 'localhost',
-              port: 7768
+              port: process.env.MONEYD_BIND_PORT || 7768
             },
             allowedOrigins: this.allowedOrigins
           }
